105827ae101ee811594e040171abda7ae41170f5
Michal Volf <chess.volf@centrum.cz>
2021-01-29 08:22:58 +0100

    Tak už se do prdele rozjeď!

--- BinarySearch.py
+++ BinarySearch.py
@@ -12,7 +12,7 @@ def binary_search_rec(lst: list, key: int, vlevo, vpravo):
         return median, lst[median]
 
     elif (vlevo == vpravo):
-        print(key, " není v seznamu")
+        print(key, " není v seznamu!")
         return False
     else:
 
@@ -20,13 +20,8 @@ def binary_search_rec(lst: list, key: int, vlevo, vpravo):
             binary_search_rec(lst, key, vlevo, median)
 
         elif key > lst[median]:
-            if vlevo == (vpravo - 1):
-                vlevo = vpravo
-                binary_search_rec(lst, key, vlevo, vpravo)
-            else:
-                levy = median
-                pravy = vpravo
-                binary_search_rec(lst, key, levy, pravy)
+            binary_search_rec(lst, key, median+1, vpravo)
+
 
 
 pole_cisel = []
