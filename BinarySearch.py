13fa1a3eedda0dd541aa0ed0727b0965f1890e03
Michal Volf <chess.volf@centrum.cz>
2021-01-29 09:42:57 +0100

    Přidal jsem úroveň rekurze.

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,4 +1,6 @@
-def binary_search_rec(lst: list, key: int, vlevo, vpravo):
+def binary_search_rec(lst: list, key: int, vlevo, vpravo, recursion_level=0):
+    recursion_level+=1
+    print("Úroveň rekurze: ", recursion_level)
     median = int(((vlevo + vpravo)) / 2)
     print("Hledaná hodnota:", key)
     print("Levý index: ", vlevo, "Index Mediánu:", median, "Pravý index:", vpravo)
@@ -9,18 +11,18 @@ def binary_search_rec(lst: list, key: int, vlevo, vpravo):
         print("Prvek nalezen!")
         print("Index prvku: ", median)
         print("Hodnota prvku: ", lst[median])
-        return True, median
+        return True
 
     elif (vlevo == vpravo):
         print(key, " není v seznamu!")
-        return False, -1
+        return False
     else:
 
         if key < lst[median]:
-            binary_search_rec(lst, key, vlevo, median)
+            binary_search_rec(lst, key, vlevo, median,recursion_level)
 
         elif key > lst[median]:
-            binary_search_rec(lst, key, median+1, vpravo)
+            binary_search_rec(lst, key, median+1, vpravo,recursion_level)
 
 
 
@@ -32,4 +34,5 @@ k=[-1000,-999,-996,-995,-4,-3,-2,-1,0,1,2,3,555,996,997,998,999,1000,1001]
 for j in enumerate(k):
     print("-----------------------------------------")
     l,m=j
-    binary_search_rec(pole_cisel, m, 0, len(pole_cisel) - 1)
+    n=binary_search_rec(pole_cisel, m, 0, len(pole_cisel) - 1)
+    print(n)
